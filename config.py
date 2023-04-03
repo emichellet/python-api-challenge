8363f735aa141cad3a57d2819105028dd432512f
emichellet <emichelletan@gmail.com>
2023-04-03 15:32:07 -0400

    updated commit

--- config.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "45c68f121e8fc24310fa128c1fb49c80"
-weather_api_key
-
-# Geoapify API Key
-geoapify_key = "60283c9022964994841b49f426654ada"
-geoapify_key
